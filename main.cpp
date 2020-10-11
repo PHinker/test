3eb7e5af6cf5088bebed0ea6e2eae0059e0dc9f0
Hanson, Alex <alex.hanson@mines.sdsmt.edu>
2020-10-11 11:59:00 -0600

    reordered main function to be one entry point and one exit.

--- main.cpp
+++ main.cpp
@@ -4,21 +4,23 @@
 int main(int argc, char **argv){
 	FileStream fs;
 	//check command args
-	if(!fs.checkCommandArgs(argc, argv))
-		fs.usage();
-
-	//open files
-	fs.openFiles(argc, argv);
-	
-
-	//call either encoder or decoder
-	if(fs.getFlag() == 'e'){
-		encode(fs);//encode
+	if(fs.checkCommandArgs(argc, argv)){
+		//open files
+		fs.openFiles(argc, argv);
+		
+		//call either encoder or decoder
+		if(fs.getFlag() == 'e'){
+			encode(fs);//encode
+		}else{
+			//check signature
+			if(fs.validateSignature()){
+				decode(fs);//decode
+			}else{
+				fs.quit(2);
+			}
+		}
 	}else{
-		//check signature
-		if(!fs.validateSignature())
-			fs.quit(2);
-		decode(fs);//decode
+		fs.usage();
 	}
 
 	fs.closeFiles();
